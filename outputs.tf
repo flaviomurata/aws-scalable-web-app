df8124a35b12319bad1b3b73e5343ede05cd8df1
Flavio Murata <flaviomurata02@gmail.com>
2026-08-13 14:40:35 -0300

    fix(terraform): correct subnet output names

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@ output "vpc_id" {
   value       = module.networking.vpc_id
 }
 
-output "public_subnet_id" {
+output "public_subnet_ids" {
   description = "IDs of the public subnets."
   value       = module.networking.public_subnet_ids
 }
 
-output "private_db_subnet_ids" {
+output "private_subnet_ids" {
   description = "IDs of the private subnets."
   value       = module.networking.private_subnet_ids
 }
