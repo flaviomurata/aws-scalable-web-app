b81bfbabf1cccbdf59c653783985fe45e1378794
Flavio Murata <flaviomurata02@gmail.com>
2026-08-17 12:10:49 -0300

    ci: add validation workflow and aws oidc plan

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -4,16 +4,13 @@ locals {
   state_bucket_name = "${var.project_name}-${data.aws_caller_identity.current.account_id}-${var.aws_region}-tfstate"
 }
 
+locals {
+  terraform_state_key = "${var.project_name}/dev/terraform.tfstate"
+}
+
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = local.state_bucket_name
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = local.state_bucket_name
-  }
+  bucket        = local.state_bucket_name
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
@@ -73,4 +70,144 @@ data "aws_iam_policy_document" "terraform_state" {
 resource "aws_s3_bucket_policy" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
   policy = data.aws_iam_policy_document.terraform_state.json
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+}
+
+data "aws_iam_policy_document" "github_plan_assume_role" {
+  statement {
+    sid    = "GitHubActions"
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity",
+    ]
+
+    principals {
+      type = "Federated"
+
+      identifiers = [
+        aws_iam_openid_connect_provider.github.arn,
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+
+      values = [
+        "sts.amazonaws.com",
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+
+      values = [
+        "repo:flaviomurata/aws-scalable-web-app:ref:refs/heads/main",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_plan" {
+  name = "${var.project_name}-github-plan"
+
+  description = "Read-only Terraform plan role assumed by GitHub Actions through OIDC."
+
+  assume_role_policy = data.aws_iam_policy_document.github_plan_assume_role.json
+}
+
+data "aws_iam_policy_document" "github_plan" {
+  statement {
+    sid    = "ReadTerraformInfrastructure"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:Describe*",
+      "ec2:Describe*",
+      "elasticloadbalancing:Describe*",
+      "iam:Get*",
+      "iam:List*",
+      "kms:DescribeKey",
+      "kms:ListAliases",
+      "rds:Describe*",
+      "rds:ListTagsForResource",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetResourcePolicy",
+      "secretsmanager:ListSecretVersionIds",
+      "sts:GetCallerIdentity",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ReadApplicationSecrets"
+    effect = "Allow"
+
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+
+    resources = [
+      "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.current.account_id}:secret:Mydbsecret-*",
+      "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.current.account_id}:secret:${var.project_name}-dev-rds-password-*",
+    ]
+  }
+
+  statement {
+    sid    = "ListTerraformStateBucket"
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.terraform_state.arn,
+    ]
+  }
+
+  statement {
+    sid    = "ReadTerraformState"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.terraform_state.arn}/${local.terraform_state_key}",
+    ]
+  }
+
+  statement {
+    sid    = "ManageTerraformStateLock"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.terraform_state.arn}/${local.terraform_state_key}.tflock",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "github_plan" {
+  name = "${var.project_name}-terraform-plan"
+  role = aws_iam_role.github_plan.name
+
+  policy = data.aws_iam_policy_document.github_plan.json
 }
--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -2,3 +2,8 @@ output "state_bucket_name" {
   description = "Name of the S3 bucket used for Terraform remote state."
   value       = aws_s3_bucket.terraform_state.id
 }
+
+output "github_plan_role_arn" {
+  description = "IAM role assumed by GitHub Actions when running Terraform plans."
+  value       = aws_iam_role.github_plan.arn
+}
